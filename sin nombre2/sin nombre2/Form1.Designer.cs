50335714af7d6e7ab6aa3eca676df4ffc0281e77
Delinson25 <101377077+Delinson25@users.noreply.github.com>
2022-08-05 15:41:36 -0400

    Add files via upload

---
+++ sin nombre2/sin nombre2/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace sin_nombre2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_6 = new System.Windows.Forms.TextBox();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.txt_13 = new System.Windows.Forms.TextBox();
+            this.txt_12 = new System.Windows.Forms.TextBox();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.txt_9 = new System.Windows.Forms.TextBox();
+            this.txt_4 = new System.Windows.Forms.TextBox();
+            this.txt_11 = new System.Windows.Forms.TextBox();
+            this.txt_15 = new System.Windows.Forms.TextBox();
+            this.txt_7 = new System.Windows.Forms.TextBox();
+            this.txt_8 = new System.Windows.Forms.TextBox();
+            this.txt_3 = new System.Windows.Forms.TextBox();
+            this.txt_10 = new System.Windows.Forms.TextBox();
+            this.txt_5 = new System.Windows.Forms.TextBox();
+            this.txt_14 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum3 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum2 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum1 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum7 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum8 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum6 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum5 = new System.Windows.Forms.TextBox();
+            this.txt_totalcolum4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_6
+            // 
+            this.txt_6.Location = new System.Drawing.Point(231, 126);
+            this.txt_6.Multiline = true;
+            this.txt_6.Name = "txt_6";
+            this.txt_6.Size = new System.Drawing.Size(106, 36);
+            this.txt_6.TabIndex = 1;
+            this.txt_6.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_2
+            // 
+            this.txt_2.Location = new System.Drawing.Point(126, 91);
+            this.txt_2.Multiline = true;
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(106, 36);
+            this.txt_2.TabIndex = 2;
+            // 
+            // txt_13
+            // 
+            this.txt_13.Location = new System.Drawing.Point(19, 227);
+            this.txt_13.Multiline = true;
+            this.txt_13.Name = "txt_13";
+            this.txt_13.Size = new System.Drawing.Size(106, 36);
+            this.txt_13.TabIndex = 3;
+            // 
+            // txt_12
+            // 
+            this.txt_12.Location = new System.Drawing.Point(231, 194);
+            this.txt_12.Multiline = true;
+            this.txt_12.Name = "txt_12";
+            this.txt_12.Size = new System.Drawing.Size(106, 36);
+            this.txt_12.TabIndex = 4;
+            this.txt_12.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(19, 91);
+            this.txt_1.Multiline = true;
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(106, 36);
+            this.txt_1.TabIndex = 5;
+            // 
+            // txt_9
+            // 
+            this.txt_9.Location = new System.Drawing.Point(231, 161);
+            this.txt_9.Multiline = true;
+            this.txt_9.Name = "txt_9";
+            this.txt_9.Size = new System.Drawing.Size(106, 36);
+            this.txt_9.TabIndex = 6;
+            this.txt_9.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txt_4
+            // 
+            this.txt_4.Location = new System.Drawing.Point(19, 126);
+            this.txt_4.Multiline = true;
+            this.txt_4.Name = "txt_4";
+            this.txt_4.Size = new System.Drawing.Size(106, 36);
+            this.txt_4.TabIndex = 7;
+            // 
+            // txt_11
+            // 
+            this.txt_11.Location = new System.Drawing.Point(126, 194);
+            this.txt_11.Multiline = true;
+            this.txt_11.Name = "txt_11";
+            this.txt_11.Size = new System.Drawing.Size(106, 36);
+            this.txt_11.TabIndex = 8;
+            // 
+            // txt_15
+            // 
+            this.txt_15.Location = new System.Drawing.Point(231, 227);
+            this.txt_15.Multiline = true;
+            this.txt_15.Name = "txt_15";
+            this.txt_15.Size = new System.Drawing.Size(106, 36);
+            this.txt_15.TabIndex = 9;
+            // 
+            // txt_7
+            // 
+            this.txt_7.Location = new System.Drawing.Point(19, 161);
+            this.txt_7.Multiline = true;
+            this.txt_7.Name = "txt_7";
+            this.txt_7.Size = new System.Drawing.Size(106, 36);
+            this.txt_7.TabIndex = 10;
+            // 
+            // txt_8
+            // 
+            this.txt_8.Location = new System.Drawing.Point(126, 161);
+            this.txt_8.Multiline = true;
+            this.txt_8.Name = "txt_8";
+            this.txt_8.Size = new System.Drawing.Size(106, 36);
+            this.txt_8.TabIndex = 11;
+            this.txt_8.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // txt_3
+            // 
+            this.txt_3.Location = new System.Drawing.Point(231, 91);
+            this.txt_3.Multiline = true;
+            this.txt_3.Name = "txt_3";
+            this.txt_3.Size = new System.Drawing.Size(106, 36);
+            this.txt_3.TabIndex = 12;
+            // 
+            // txt_10
+            // 
+            this.txt_10.Location = new System.Drawing.Point(19, 194);
+            this.txt_10.Multiline = true;
+            this.txt_10.Name = "txt_10";
+            this.txt_10.Size = new System.Drawing.Size(106, 36);
+            this.txt_10.TabIndex = 13;
+            // 
+            // txt_5
+            // 
+            this.txt_5.Location = new System.Drawing.Point(126, 126);
+            this.txt_5.Multiline = true;
+            this.txt_5.Name = "txt_5";
+            this.txt_5.Size = new System.Drawing.Size(106, 36);
+            this.txt_5.TabIndex = 14;
+            // 
+            // txt_14
+            // 
+            this.txt_14.Location = new System.Drawing.Point(126, 227);
+            this.txt_14.Multiline = true;
+            this.txt_14.Name = "txt_14";
+            this.txt_14.Size = new System.Drawing.Size(106, 36);
+            this.txt_14.TabIndex = 15;
+            // 
+            // txt_totalcolum3
+            // 
+            this.txt_totalcolum3.Location = new System.Drawing.Point(231, 269);
+            this.txt_totalcolum3.Name = "txt_totalcolum3";
+            this.txt_totalcolum3.ReadOnly = true;
+            this.txt_totalcolum3.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum3.TabIndex = 17;
+            // 
+            // txt_totalcolum2
+            // 
+            this.txt_totalcolum2.Location = new System.Drawing.Point(126, 269);
+            this.txt_totalcolum2.Name = "txt_totalcolum2";
+            this.txt_totalcolum2.ReadOnly = true;
+            this.txt_totalcolum2.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum2.TabIndex = 18;
+            // 
+            // txt_totalcolum1
+            // 
+            this.txt_totalcolum1.Location = new System.Drawing.Point(19, 269);
+            this.txt_totalcolum1.Name = "txt_totalcolum1";
+            this.txt_totalcolum1.ReadOnly = true;
+            this.txt_totalcolum1.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum1.TabIndex = 19;
+            this.txt_totalcolum1.TextChanged += new System.EventHandler(this.textBox17_TextChanged);
+            // 
+            // txt_totalcolum7
+            // 
+            this.txt_totalcolum7.Location = new System.Drawing.Point(356, 210);
+            this.txt_totalcolum7.Name = "txt_totalcolum7";
+            this.txt_totalcolum7.ReadOnly = true;
+            this.txt_totalcolum7.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum7.TabIndex = 20;
+            // 
+            // txt_totalcolum8
+            // 
+            this.txt_totalcolum8.Location = new System.Drawing.Point(356, 243);
+            this.txt_totalcolum8.Name = "txt_totalcolum8";
+            this.txt_totalcolum8.ReadOnly = true;
+            this.txt_totalcolum8.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum8.TabIndex = 21;
+            // 
+            // txt_totalcolum6
+            // 
+            this.txt_totalcolum6.Location = new System.Drawing.Point(356, 177);
+            this.txt_totalcolum6.Name = "txt_totalcolum6";
+            this.txt_totalcolum6.ReadOnly = true;
+            this.txt_totalcolum6.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum6.TabIndex = 22;
+            // 
+            // txt_totalcolum5
+            // 
+            this.txt_totalcolum5.Location = new System.Drawing.Point(356, 142);
+            this.txt_totalcolum5.Name = "txt_totalcolum5";
+            this.txt_totalcolum5.ReadOnly = true;
+            this.txt_totalcolum5.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum5.TabIndex = 23;
+            // 
+            // txt_totalcolum4
+            // 
+            this.txt_totalcolum4.Location = new System.Drawing.Point(356, 107);
+            this.txt_totalcolum4.Name = "txt_totalcolum4";
+            this.txt_totalcolum4.ReadOnly = true;
+            this.txt_totalcolum4.Size = new System.Drawing.Size(106, 20);
+            this.txt_totalcolum4.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "TABLA  SUMA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 320);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_totalcolum4);
+            this.Controls.Add(this.txt_totalcolum5);
+            this.Controls.Add(this.txt_totalcolum6);
+            this.Controls.Add(this.txt_totalcolum8);
+            this.Controls.Add(this.txt_totalcolum7);
+            this.Controls.Add(this.txt_totalcolum1);
+            this.Controls.Add(this.txt_totalcolum2);
+            this.Controls.Add(this.txt_totalcolum3);
+            this.Controls.Add(this.txt_14);
+            this.Controls.Add(this.txt_5);
+            this.Controls.Add(this.txt_10);
+            this.Controls.Add(this.txt_3);
+            this.Controls.Add(this.txt_8);
+            this.Controls.Add(this.txt_7);
+            this.Controls.Add(this.txt_15);
+            this.Controls.Add(this.txt_11);
+            this.Controls.Add(this.txt_4);
+            this.Controls.Add(this.txt_9);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.txt_12);
+            this.Controls.Add(this.txt_13);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.txt_6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_6;
+        private System.Windows.Forms.TextBox txt_2;
+        private System.Windows.Forms.TextBox txt_13;
+        private System.Windows.Forms.TextBox txt_12;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.TextBox txt_9;
+        private System.Windows.Forms.TextBox txt_4;
+        private System.Windows.Forms.TextBox txt_11;
+        private System.Windows.Forms.TextBox txt_15;
+        private System.Windows.Forms.TextBox txt_7;
+        private System.Windows.Forms.TextBox txt_8;
+        private System.Windows.Forms.TextBox txt_3;
+        private System.Windows.Forms.TextBox txt_10;
+        private System.Windows.Forms.TextBox txt_5;
+        private System.Windows.Forms.TextBox txt_14;
+        private System.Windows.Forms.TextBox txt_totalcolum3;
+        private System.Windows.Forms.TextBox txt_totalcolum2;
+        private System.Windows.Forms.TextBox txt_totalcolum1;
+        private System.Windows.Forms.TextBox txt_totalcolum7;
+        private System.Windows.Forms.TextBox txt_totalcolum8;
+        private System.Windows.Forms.TextBox txt_totalcolum6;
+        private System.Windows.Forms.TextBox txt_totalcolum5;
+        private System.Windows.Forms.TextBox txt_totalcolum4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sin nombre2/sin nombre2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sin_nombre2
+{
+  
+    public partial class Form1 : Form
+    {
+        int numero1;
+        int numero2;
+        int numero3;
+        int numero4;
+        int numero5;
+        int numero6;
+        int numero7;
+        int numero8;
+        int numero9;
+        int numero10;
+        int numero11;
+        int numero12;
+        int numero13;
+        int numero14;
+        int numero15;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox17_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
